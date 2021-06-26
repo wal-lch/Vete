f353e71054d0a86f0cb5393a30fe2c004106aab3
wal-lch <wal.leon@outlook.com>
2021-06-26 14:29:49 -0600

    mi primer commit

---
+++ Models/Mascota.cs
@@ -0,0 +1,10 @@
+namespace Vete.Models
+{
+    public class Mascota
+    {
+        public int Id{get; set;}
+        public string Nombre{get;set;}
+        public int Edad {get;set;}
+        public string  Dueño {get; set;} 
+    } 
+}
